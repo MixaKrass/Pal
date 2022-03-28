530a8580b1607cdaf317bfcc8eab69351e64dbb6
MixaKrass <2584275@gmail.com>
2022-03-28 15:21:58 +0300

    сделал весь header

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
-const defaultBackgroundPhoto = 'images/menu.svg';
 const services = document.querySelector('.services__container');
 const servicesTitle = document.querySelector('.services__title');
-const menu = document.querySelector('.menu__nav');
+const head = document.querySelector('.header');
 const items = document.querySelectorAll('.menu__item');
 
 for (let item of items) {
   item.addEventListener('mouseenter', function(evt) {
     const name = evt.target.dataset.name;
-    menu.style.backgroundImage = `url(images/background/services__subtitle_0.svg)`;
-
+    head.style.backgroundImage = `url(images/background/${name}.svg)`;
+    servicesTitle.classList.add(name);
+    head.classList.add('white');
+    services.classList.add('white');
   })
 
+  item.addEventListener('mouseleave', function() {
+    head.style.backgroundImage= '';
+    servicesTitle.classList.remove('uslugi', 'works', 'design', 'remont')
+    head.classList.remove('white');
+    services.classList.remove('white');
+  })
 }
 
